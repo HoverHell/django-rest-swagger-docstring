5081a9fc63e110f936b3fd860c042ce026fb984c
Jan Romaniak <romaniakjan@gmail.com>
2017-03-14 11:21:52 +0100

    not abort on errors

--- django_rest_swagger_docstring/schemas.py
+++ django_rest_swagger_docstring/schemas.py
@@ -26,11 +26,26 @@ types_lookup = ClassLookupDict({
 class DocsSchemaGenerator(SchemaGenerator):
     def get_link(self, path, method, view):
         """ Return a `coreapi.Link` instance for the given endpoint. """
-        fields = self.get_path_fields(path, method, view)
-        fields += self.get_serializer_fields(path, method, view)
-        fields += self.get_pagination_fields(path, method, view)
-        fields += self.get_filter_fields(path, method, view)
-        fields += self.get_docs_fields(path, method, view)
+        try:
+            fields = self.get_path_fields(path, method, view)
+        except:
+            pass
+        try:
+            fields += self.get_serializer_fields(path, method, view)
+        except:
+            pass
+        try:
+            fields += self.get_pagination_fields(path, method, view)
+        except:
+            pass
+        try:
+            fields += self.get_filter_fields(path, method, view)
+        except:
+            pass
+        try:
+            fields += self.get_docs_fields(path, method, view)
+        except:
+            pass
 
         if fields and any([field.location in ('form', 'body') for field in fields]):
             encoding = self.get_encoding(path, method, view)
